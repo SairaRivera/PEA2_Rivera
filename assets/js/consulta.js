0d1523107ca34300e46ccb7c2dd42e3e7f28bf69
Saira Rivera <saranikoleriveragarcia@gmail.com>
2022-10-04 11:37:39 -0500

    PEA2

---
+++ assets/js/consulta.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciar();
+})
+
+function iniciar(){
+    obtenerClientes();
+}
+
+async function obtenerClientes(){
+const url = "data/consulta.php?accion=listar";
+const respuesta = await fetch (url);
+const resultado = await respuesta.json();
+if(resultado.codigo === 200){
+    const data = resultado.data;
+    let html  ="";
+    data.forEach(cliente => {
+        
+        html += "<tr>";
+        html += "<td>"+ cliente.id + "</td>";
+        html += "<td>"+ cliente.razon_social + "</td>";
+        html += "<td>"+ cliente.numero_documento + "</td>";
+        html += "<td>"+ cliente.direccion + "</td>";
+        html += "</tr>";
+    });
+     const tabla = document.querySelector("#tblConsulta");
+     tabla.innerHTML = html
+     console.log(tabla);
+     
+    }
+}
